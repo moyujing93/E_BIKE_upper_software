83cd888bcb3da50cbd31f445f53c551d5c45a9a4
user1 <1192493678@qq.com>
2024-10-04 21:45:11 +0800

    修改RS485协议成功111

--- User/main.c
+++ User/main.c
@@ -31,7 +31,7 @@
 
 int main(void)
 {
-    
+      
     HAL_Init();                                 /* 初始化HAL库 */
     sys_stm32_clock_init(RCC_PLL_MUL9);         /* 设置时钟,72M */
     delay_init(72);                             /* 初始化延时函数 */
